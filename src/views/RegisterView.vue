321ddeb2175a08ac5354c47e9b1e7c2613674196
Charles <charleslanop@gmail.com>
2024-06-14 16:07:22 -0300

    updated

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+// import router from '@/router';
+import { computed, ref } from 'vue';
+import AlertComponent from '@/components/AlertComponent.vue';
 
 const username = ref('');
 const password = ref('');
@@ -7,9 +9,37 @@ const email = ref('');
 const fullName = ref('');
 const gender = ref<'m' | 'f'>('m');
 const termsAgreed = ref(true);
+const usernameRequired = ref(false);
+const passwordRequired = ref(false);
+const emailRequired = ref(false);
+const fullNameRequired = ref(false);
+const isDisabled = ref(false);
+const alertMessage = ref('');
+const isSuccess = ref(false);
+
+const isRegisterDisabled = computed(() => {
+  return termsAgreed.value === false || isDisabled.value === true;
+});
 
 function register(): void {
-  alert(username.value);
+  usernameRequired.value = true;
+  passwordRequired.value = true;
+  emailRequired.value = true;
+  fullNameRequired.value = true;
+  if (!username.value || !password.value || !email.value || !fullName.value) {
+    return;
+  }
+  isDisabled.value = true;
+  setTimeout(() => {
+    const error = false;
+    if (error) {
+      isDisabled.value = false;
+      alertMessage.value = 'Error';
+      return;
+    }
+    isSuccess.value = true;
+    // router.push({ name: 'register-success' });
+  }, 1000);
 }
 
 console.log('%c Warning', 'font-size: 2em; color: yellow;');
@@ -22,7 +52,7 @@ console.log('%c All good!', 'font-size: 2em; color: green;');
     <div class="content">
       <div class="container">
         <div class="box register-box">
-          <div class="register-background pt-3">
+          <div class="register-background pt-3" v-if="!isSuccess">
             <div class="columns is-centered">
               <div class="column is-one-third">
                 <form @submit.prevent="register">
@@ -36,8 +66,12 @@ console.log('%c All good!', 'font-size: 2em; color: green;');
                           type="text"
                           placeholder="username"
                           v-model.trim="username"
+                          :class="{ 'is-danger': usernameRequired && !username }"
                         />
                       </div>
+                      <p class="help is-danger" v-if="usernameRequired && !username">
+                        O nome do usuário é obrigatório
+                      </p>
                     </div>
                     <div class="field">
                       <label class="label">Senha</label>
@@ -47,8 +81,12 @@ console.log('%c All good!', 'font-size: 2em; color: green;');
                           type="password"
                           placeholder="senha"
                           v-model.trim="password"
+                          :class="{ 'is-danger': passwordRequired && !password }"
                         />
                       </div>
+                      <p class="help is-danger" v-if="passwordRequired && !password">
+                        A senha é obrigatória
+                      </p>
                     </div>
                     <div class="field">
                       <label class="label">E-mail</label>
@@ -58,8 +96,12 @@ console.log('%c All good!', 'font-size: 2em; color: green;');
                           type="email"
                           placeholder="e-mail"
                           v-model.trim="email"
+                          :class="{ 'is-danger': emailRequired && !email }"
                         />
                       </div>
+                      <p class="help is-danger" v-if="emailRequired && !email">
+                        O email é obrigatório
+                      </p>
                     </div>
                   </div>
                   <div class="box">
@@ -69,11 +111,15 @@ console.log('%c All good!', 'font-size: 2em; color: green;');
                       <div class="control">
                         <input
                           class="input is-shadowless is-borderless"
-                          type="email"
+                          type="text"
                           placeholder="nome completo"
                           v-model.trim="fullName"
+                          :class="{ 'is-danger': fullNameRequired && !fullName }"
                         />
                       </div>
+                      <p class="help is-danger" v-if="fullNameRequired && !fullName">
+                        O nome completo é obrigatório
+                      </p>
                     </div>
                     <div class="field">
                       <label class="label">Gênero</label>
@@ -94,9 +140,38 @@ console.log('%c All good!', 'font-size: 2em; color: green;');
                         Li e concordo com os Termos de Uso do jogo.
                       </label>
                     </div>
-                    <button class="button is-primary">Continuar registro</button>
+                    <button
+                      class="button is-primary"
+                      :class="{ 'is-loading': isRegisterDisabled }"
+                      :disabled="isRegisterDisabled"
+                    >
+                      Continuar registro
+                    </button>
                   </div>
                 </form>
+              </div>
+            </div>
+          </div>
+          <div class="pt-3 mt-5" v-else>
+            <div class="box register-box-success">
+              <div class="columns">
+                <div class="column">
+                  <figure>
+                    <img
+                      src="../assets/images/register/register-nami.png"
+                      alt="Register nami image"
+                    />
+                  </figure>
+                </div>
+                <div class="column">
+                  <h1>Registrar</h1>
+                  <p>
+                    Seu cadastro foi realizado no com sucesso. Entre agora e crie seu personagem.
+                  </p>
+                  <button class="button is-warning" @click="$router.push({ name: 'home' })">
+                    Fazer login
+                  </button>
+                </div>
               </div>
             </div>
           </div>
@@ -110,6 +185,7 @@ console.log('%c All good!', 'font-size: 2em; color: green;');
       </div>
     </footer>
   </div>
+  <AlertComponent :message="alertMessage" @close="alertMessage = ''" />
 </template>
 
 <style scoped>
@@ -157,5 +233,11 @@ console.log('%c All good!', 'font-size: 2em; color: green;');
   background-position: center;
   width: 100%;
   height: 175px;
+}
+
+.register-box-success {
+  background-image: url(../assets/images/register/register-pattern.png);
+  background-repeat: repeat;
+  background-size: 6px 39px;
 }
 </style>
